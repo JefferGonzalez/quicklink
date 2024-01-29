d96e393f4d75a16bf81c8f2d51307706a2f297c8
JefferGonzalez <jefferd2016@gmail.com>
2024-01-28 21:12:01 -0500

    feat: 🥳 Add logic to create slugs in free tier

--- src/pages/GettingStarted.tsx
+++ src/pages/GettingStarted.tsx
@@ -1,11 +1,15 @@
+import { API_URL, APP_URL } from '@/Config'
 import Layout from '@/components/Layout'
 import SlugForm from '@/components/SlugForm'
 import { Button } from '@/components/ui/button'
 import { UrlSlug, UrlSlugSchema } from '@/schemas/UrlSlug'
+import { Response } from '@/types'
 import { zodResolver } from '@hookform/resolvers/zod'
+import confetti from 'canvas-confetti'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
 
 export default function GettingStarted(): JSX.Element {
   const [loading, setLoading] = useState(false)
@@ -21,8 +25,68 @@ export default function GettingStarted(): JSX.Element {
 
   const handleSubmit = async (values: UrlSlug) => {
     setLoading(true)
-    console.log(values)
+    if (values.url === values.slug) {
+      form.setError('slug', {
+        type: 'pattern',
+        message: 'The slug cannot be the same as the URL.'
+      })
+      setLoading(false)
+      return
+    }
+
+    const response = await fetch(`${API_URL}/api/slug`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(values)
+    })
+
+    const { errors }: Response = await response.json()
+
+    if (!response.ok) {
+      const statusCode = response.status
+      if (statusCode === 400) {
+        for (const { message, path } of errors) {
+          const name = path?.at(0) ?? 'root'
+
+          form.setError(name, {
+            type: 'pattern',
+            message
+          })
+        }
+      }
+
+      if (statusCode === 409) {
+        form.setError('slug', {
+          type: 'pattern',
+          message: errors.at(0)?.message || 'The slug is already taken.'
+        })
+      }
+
+      setLoading(false)
+      return
+    }
+
+    form.reset()
+
     setLoading(false)
+
+    confetti({
+      particleCount: 200,
+      spread: 70,
+      origin: { y: 0.6 }
+    })
+
+    toast('🎉 Slug created successfully!', {
+      duration: 10000,
+      action: {
+        label: 'Open slug',
+        onClick: () => {
+          window.open(`${APP_URL}/s/${values.slug}`, '_blank')
+        }
+      }
+    })
   }
 
   return (
