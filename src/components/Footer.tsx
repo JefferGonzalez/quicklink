d08719c3e095c2f4f3c8a4fb2889da8295c1a948
JefferGonzalez <jefferd2016@gmail.com>
2024-02-24 17:32:27 -0500

    feat: 🥳 Update footer information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,31 @@ export default function Footer(): JSX.Element {
   return (
     <footer className='fixed bottom-0 w-full py-4'>
       <div className='container max-w-[1100px] flex items-center justify-between'>
-        <span className='bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500'>
-          Made using PERN Stack
-        </span>
+        <div className='flex  items-center space-x-2'>
+          <span className='text-lg bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500'>
+            Made using PERN Stack | Inspired by{' '}
+          </span>
+
+          <img
+            loading='lazy'
+            className='rounded-full shadow-lg size-8 cursor-pointer'
+            src='midudev.webp'
+            title='midudev'
+            alt='Miguel Ángel Durán'
+            onClick={() => window.open('https://github.com/midudev', '_blank')}
+          ></img>
+          <img
+            loading='lazy'
+            className='rounded-full shadow-lg size-8 cursor-pointer'
+            src='pheralb.webp'
+            title='pheralb'
+            alt='Pablo Hernández Albertos'
+            onClick={() => window.open('https://github.com/pheralb', '_blank')}
+          ></img>
+        </div>
 
         <a href='' title='GitHub'>
-          <GitHubIcon />
+          <GitHubIcon className='size-8' />
           <span className='sr-only'>GitHub</span>
         </a>
       </div>
