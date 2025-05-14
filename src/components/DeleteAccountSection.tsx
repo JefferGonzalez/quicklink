d93e75addfdfb3af6b84e65005c374820e03c835
JefferGonzalez <jefferd2016@gmail.com>
2025-05-13 21:33:52 -0500

    feat: 🥳 Implement DeleteAccountSection component for account deletion functionality

---
+++ src/components/DeleteAccountSection.tsx
@@ -0,0 +1,129 @@
+import DeleteAccountForm from '@/components/DeleteAccountForm'
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent } from '@/components/ui/dialog'
+import { Separator } from '@/components/ui/separator'
+import { AuthContext } from '@/context/AuthContext'
+import {
+  createDeleteAccountValidationSchema as DeleteAccountValidationSchema,
+  type DeleteAccountValidation
+} from '@/schemas/DeleteAccountValidation'
+import { deleteUserAccount, signOut } from '@/services/User'
+import type { User } from '@/types'
+import { showToastError } from '@/utils/errors'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Fragment, useContext, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+interface DeleteAccountSectionProps {
+  user?: User
+}
+
+export default function DeleteAccountSection({
+  user
+}: DeleteAccountSectionProps): JSX.Element {
+  const { logout } = useContext(AuthContext)
+  const [showDisclaimer, setShowDisclaimer] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const form = useForm<DeleteAccountValidation>({
+    resolver: zodResolver(DeleteAccountValidationSchema(user?.username)),
+    defaultValues: {
+      username: '',
+      text: ''
+    }
+  })
+
+  const handleSubmit = async () => {
+    setLoading(true)
+
+    try {
+      const response = await deleteUserAccount()
+
+      const statusCode = response.status
+      if (!response.ok) {
+        if (statusCode === 401) logout()
+
+        if (statusCode === 404) await signOut()
+      }
+
+      if (statusCode === 204) {
+        setShowDisclaimer(false)
+
+        toast('🥲 Account deleted successfully. Redirecting...')
+
+        setTimeout(() => logout(), 3000)
+      }
+
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+
+      showToastError()
+    }
+  }
+
+  const handleCancel = () => {
+    form.clearErrors()
+    form.reset()
+
+    setShowDisclaimer(false)
+  }
+
+  const handleShowDisclaimer = (boolean: boolean) => {
+    if (loading) return
+
+    setShowDisclaimer(boolean)
+  }
+
+  return (
+    <Fragment>
+      <section className='rounded-md border border-red-600 shadow-md bg-neutral-900 text-neutral-100'>
+        <div className='p-4 flex flex-col gap-y-4'>
+          <h4 className='text-xl font-semibold'>Delete Account</h4>
+
+          <p className='text-pretty'>
+            Permanently delete your account. This action is not reversible, so
+            proceed with caution. All your data will be lost.
+          </p>
+        </div>
+
+        <footer className='bg-neutral-950 rounded-b-md p-4 flex justify-end gap-x-4'>
+          <Button
+            title='Delete Account'
+            variant='destructive'
+            onClick={() => setShowDisclaimer(true)}
+          >
+            <span className='sr-only'>Delete Account</span>
+            Delete Account
+          </Button>
+        </footer>
+      </section>
+
+      <Dialog open={showDisclaimer} onOpenChange={handleShowDisclaimer}>
+        <DialogContent
+          className='overflow-hidden p-0 shadow-lg border-neutral-700 '
+          onInteractOutside={(e) => e.preventDefault()}
+        >
+          <section className='bg-black p-8'>
+            <h4 className='text-2xl font-bold mb-6'>Delete Account</h4>
+
+            <p className='text-pretty p-1 rounded-md bg-red-800'>
+              This action is not reversible. Please be sure.
+            </p>
+
+            <Separator className='my-4 bg-neutral-500' />
+
+            <DeleteAccountForm
+              username={user?.username}
+              form={form}
+              loading={loading}
+              handleSubmit={handleSubmit}
+              handleCancel={handleCancel}
+            />
+          </section>
+        </DialogContent>
+      </Dialog>
+    </Fragment>
+  )
+}
